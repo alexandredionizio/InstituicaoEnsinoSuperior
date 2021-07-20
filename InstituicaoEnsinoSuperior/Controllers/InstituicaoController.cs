b6af11127fa5fb2fe8a6fd2066586cd594d2fd4e
Alexandre Dio <alexandre.dio@gmail.com>
2021-07-19 22:25:33 -0300

    Add project files.

---
+++ InstituicaoEnsinoSuperior/Controllers/InstituicaoController.cs
@@ -0,0 +1,52 @@
+﻿using InstituicaoEnsinoSuperior.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InstituicaoEnsinoSuperior.Controllers
+{
+    public class InstituicaoController : Controller
+    {
+        private static IList<Instituicao> instituicoes = new List<Instituicao>()
+        {
+            new Instituicao()
+            {
+                InstituicaoID = 1,
+                Nome = "UniParaná",
+                Endereco = "Paraná"
+            },
+            new Instituicao()
+            {
+                InstituicaoID = 2,
+                Nome = "UniSanta",
+                Endereco = "Santa Catarina"
+            },
+            new Instituicao()
+            {
+                InstituicaoID = 3,
+                Nome = "UniSãoPaulo",
+                Endereco = "São Paulo"
+            },
+            new Instituicao()
+            {
+                InstituicaoID = 4,
+                Nome = "UniSulGrandense",
+                Endereco = "Rio Grande do Sul"
+            },
+            new Instituicao()
+            {
+                InstituicaoID = 5,
+                Nome = "UniCarioca",
+                Endereco = "Rio de Janeiro"
+            }
+        };
+        
+        public IActionResult Index()
+        {
+            //return View(instituicoes);
+            return View(instituicoes.OrderBy(i => i.Nome));
+        }
+    }
+}
