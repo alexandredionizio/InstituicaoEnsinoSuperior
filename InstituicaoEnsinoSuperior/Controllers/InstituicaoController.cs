02daa905a33f130c88bf9e38361f5b0370488516
Alexandre Dio <alexandre.dio@gmail.com>
2021-07-20 23:35:28 -0300

    Create: Visão e Action Create

--- InstituicaoEnsinoSuperior/Controllers/InstituicaoController.cs
+++ InstituicaoEnsinoSuperior/Controllers/InstituicaoController.cs
@@ -42,11 +42,26 @@ namespace InstituicaoEnsinoSuperior.Controllers
                 Endereco = "Rio de Janeiro"
             }
         };
-        
+
         public IActionResult Index()
         {
             //return View(instituicoes);
             return View(instituicoes.OrderBy(i => i.Nome));
+        }
+
+        //Get: Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Instituicao instituicao)
+        {
+            instituicoes.Add(instituicao);
+            instituicao.InstituicaoID = instituicoes.Select(i => i.InstituicaoID).Max() + 1;
+            return RedirectToAction("Index");
         }
     }
 }
