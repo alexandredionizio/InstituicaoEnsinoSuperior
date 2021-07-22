f551c0d718ba2e5d447ad53df72b42f82dee1f57
Alexandre Dio <alexandre.dio@gmail.com>
2021-07-22 18:59:48 -0300

    Data: Contexto, DI e conexão com SGBD SQL Server

---
+++ InstituicaoEnsinoSuperior/Data/IESContext.cs
@@ -0,0 +1,31 @@
+﻿using InstituicaoEnsinoSuperior.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InstituicaoEnsinoSuperior.Data
+{
+    public class IESContext : DbContext
+    {
+        public IESContext(DbContextOptions<IESContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet <Departamento> Departamentos { get; set; }
+        public DbSet <Instituicao> instituicoes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Departamento>().ToTable("Departamento");
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=IESCasaDoCodigo;Trusted_Connection=True;MultipleActiveResultSets=True");
+        //}
+    }
+}
