091168bcb556b36e09835b734340bfd47324328e
Alexandre Dio <alexandre.dio@gmail.com>
2021-07-25 17:23:41 -0300

    Actions dos métodos Edit, Details e Remove da entidade Departamento e todas as views correspondentes.

--- InstituicaoEnsinoSuperior/Controllers/DepartamentoController.cs
+++ InstituicaoEnsinoSuperior/Controllers/DepartamentoController.cs
@@ -1,4 +1,5 @@
 ﻿using InstituicaoEnsinoSuperior.Data;
+using InstituicaoEnsinoSuperior.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -20,6 +21,131 @@ namespace InstituicaoEnsinoSuperior.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await _context.Departamentos.OrderBy(c => c.Nome).ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Nome")] Departamento departamento)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Add(departamento);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (DbUpdateException)
+            {
+                ModelState.AddModelError("","Não foi possível inserir os dados.");
+            }
+            return View(departamento);
+        }
+
+        public async Task<IActionResult> Edit(long? id)
+        {
+            //Verifica se o id veio nulo e casop positivo gero um erro HTTP 404
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            //busca o id pela PK e verifico se encontrou. Caso negativo gero outro erro HTTP 404
+            var departamento = await _context.Departamentos.SingleOrDefaultAsync(m => m.DepartamentoID == id);
+            if (departamento == null)
+            {
+                return NotFound();
+            }
+
+            //caso encontrou o id chamo a view passando o departamento
+            return View(departamento);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit (long? id, [Bind("DepartamentoID,Nome")] Departamento departamento)
+        {
+            //Verifica se os valores recebidos para o id e DepartamentoID do objeto departamento são iguais.
+            //Caso negativo dispara o erro HTTP 404
+            if (id != departamento.DepartamentoID)
+            {
+                return NotFound();
+            }
+
+            //Verifica se o modelo é válido, ou seja, se não há nenhuma validação de erro
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(departamento);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DepartamentoExists(departamento.DepartamentoID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(departamento);
+        }
+
+        private bool DepartamentoExists(long? id)
+        {
+            return _context.Departamentos.Any(e => e.DepartamentoID == id);
+        }
+
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var departamento = await _context.Departamentos.SingleOrDefaultAsync(m => m.DepartamentoID == id);
+            if (departamento == null)
+            {
+                return NotFound();
+            }
+            return View(departamento);
+        }
+
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var departamento = await _context.Departamentos.SingleOrDefaultAsync(m => m.DepartamentoID == id);
+            if (departamento == null)
+            {
+                return NotFound();
+            }
+            return View(departamento);
+        }
+        
+        //POST: Departamento/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long? id)
+        {
+            var departamento = await _context.Departamentos.SingleOrDefaultAsync(m => m.DepartamentoID == id);
+            _context.Departamentos.Remove(departamento);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
