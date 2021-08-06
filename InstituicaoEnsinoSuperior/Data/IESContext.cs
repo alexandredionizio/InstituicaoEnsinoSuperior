0b0afc9445d6c5d7a0e81b79b71683e1ca2ec77d
Alexandre Dio <alexandre.dio@gmail.com>
2021-08-05 21:28:34 -0300

    Migrations EF Core

--- InstituicaoEnsinoSuperior/Data/IESContext.cs
+++ InstituicaoEnsinoSuperior/Data/IESContext.cs
@@ -17,10 +17,24 @@ namespace InstituicaoEnsinoSuperior.Data
         public DbSet <Departamento> Departamentos { get; set; }
         public DbSet <Instituicao> Instituicoes { get; set; }
 
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<Disciplina> Disciplinas { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<Departamento>().ToTable("Departamento");
+            modelBuilder.Entity<CursoDisciplina>()
+                .HasKey(cd => new { cd.CursoID, cd.DisciplinaID });
+
+            modelBuilder.Entity<CursoDisciplina>()
+                .HasOne(c => c.Curso)
+                .WithMany(cd => cd.CursoDisciplinas)
+                .HasForeignKey(c => c.CursoID);
+
+            modelBuilder.Entity<CursoDisciplina>()
+                .HasOne(d => d.Disciplina)
+                .WithMany(cd => cd.CursosDisciplinas)
+                .HasForeignKey(d => d.DisciplinaID);
         }
 
         //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
